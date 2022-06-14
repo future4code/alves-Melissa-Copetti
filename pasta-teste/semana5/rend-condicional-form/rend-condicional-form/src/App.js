ac52028cbfae89050fbdd030ec0203fc0b95a061
Melissa Fernanda Copetti <melissacopetti@hotmail.com>
2022-06-13 22:49:21 -0300

    criado diretório rend-condicional-form, exercício com erro e não concluído

---
+++ pasta-teste/semana5/rend-condicional-form/rend-condicional-form/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+ import DadosGerais from './components/DadosGerais';
+// import DadosComSuperior from;
+// import DadosSemSuperior from;
+// import Agradecimentos from;
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     {DadosGerais}
+    </div>
+  );
+}
+
+export default App;
---
+++ pasta-teste/semana5/rend-condicional-form/rend-condicional-form/src/components/Agradecimentos.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './App.css';
+
+function Agradecimentos() {
+  return (
+    <div className="Agradecimentos">
+     
+    </div>
+  );
+}
+
+export default Agradecimentos;
