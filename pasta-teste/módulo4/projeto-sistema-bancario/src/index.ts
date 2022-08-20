46f48a41d3f1b34b89dc98cf02a4a940853931d6
Melissa Fernanda Copetti <melissacopetti@hotmail.com>
2022-08-20 16:14:38 -0300

    requisitos mínimos cumpridos

--- pasta-teste/módulo4/projeto-sistema-bancario/src/index.ts
+++ pasta-teste/módulo4/projeto-sistema-bancario/src/index.ts
@@ -10,9 +10,20 @@ app.use(cors());
 
 app.post('/user/criate', (req: Request, res: Response)=>{
     try {
-    const {name, cpf, dateOfBithSatring} = req.body
-    const [day, month,year] = dateOfBithSatring.split("/")
+    const {name, cpf, dateOfBithString} = req.body
+    
+    const [day, month,year] = dateOfBithString.split("/")
+    
     const dateOfBirth:Date = new Date(`${year}-${month}-${day}`)
+    
+    const ageMilisseconds:number = Date.now() - dateOfBirth.getTime()
+    
+    const ageYears:number = ageMilisseconds /1000 / 60 / 60 / 24 / 360
+    if (ageYears < 18){
+        res.statusCode = 406
+        throw new Error("Usuário menor de 18 anos, é necessário ser maior");
+        
+    }
     contas.push({
         name,
         cpf,
@@ -21,12 +32,24 @@ app.post('/user/criate', (req: Request, res: Response)=>{
         estatement:[]
 
     })
-        
-    } catch (error) {
-        
+        res.status(201).send("Conta criada com sucesso")
+    } catch (error:any) {
+       res.send(error.message)
     }
 })
 
+app.get('/user/all',(req: Request, res: Response)=>{
+    try {
+        if(!contas.length){
+            res.statusCode = 404
+            throw new Error("Nenhuma conta encontrada");
+            
+        }
+        res.status(200).send(contas)
+    } catch (error:any) {
+        res.send(error.message)
+    }
+})
 
 app.listen(3003, () => {
     console.log("Server is running in http://localhost:3003")
