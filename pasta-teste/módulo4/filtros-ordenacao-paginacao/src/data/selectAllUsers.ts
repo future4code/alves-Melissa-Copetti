9ff3a777e759619905f11e9c94d7648dd058f5a7
Melissa Fernanda Copetti <melissacopetti@hotmail.com>
2022-08-30 15:35:36 -0300

    filtros-ordenação-paginação

---
+++ pasta-teste/módulo4/filtros-ordenacao-paginacao/src/data/selectAllUsers.ts
@@ -0,0 +1,18 @@
+import { user } from './../types';
+import { connection } from "./connection"
+
+export default async function selectAllUsers():Promise<any> {
+    const result = await connection("aula49_exercicio").where("name", "LIKE", `${name}`)
+    const usuarios = result.map(toUsers)
+    
+    return result[0]
+ }
+
+ const toUsers = (input:any): user => {
+    return {
+        id:input.id,
+        name:input.name,
+        email:input.email,
+        type:input.type
+    };
+ };
---
+++ pasta-teste/módulo4/filtros-ordenacao-paginacao/src/endpoints/getAllUsers.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express"
+import selectAllUsers from "../data/selectAllUsers"
+
+
+export const getAllUsers = async(req: Request,res: Response): Promise<void> =>{
+    try {
+       const users = await selectAllUsers()
+       const name = req.query.name || "";
+ 
+       if(!users.length){
+          res.statusCode = 404
+          throw new Error("No users found")
+       }
+       
+      
+ 
+       res.status(200).send(users)
+       
+    } catch (error:any) {
+       console.log(error)
+       res.send(error.message || error.sqlMessage)
+    }
+ }
