baa48256463ad6540b30e8a04efee9cb1b29e8fd
Melissa Fernanda Copetti <melissacopetti@hotmail.com>
2022-09-05 16:52:30 -0300

    classes-encapsulamento

--- pasta-teste/módulo4/classes-encapsulamento/src/index.ts
+++ pasta-teste/módulo4/classes-encapsulamento/src/index.ts
@@ -1,3 +1,4 @@
+
 //Exercício 1
 //a)o constructor serve pra inicializar uma classe, ou seja, 'botar a classe pra funcionar'.
 
@@ -9,11 +10,12 @@ class UserAccount {
   private balance: number = 0;
   private transactions: Transaction[] = [];
 
-  constructor(cpf: string, name: string, age: number) {
+  constructor(cpf: string, name: string, age: number, transactions:Transaction[]) {
     console.log("Chamando o construtor da classe UserAccount");
     this.cpf = cpf;
     this.name = name;
     this.age = age;
+    this.transactions = transactions
   }
   public getCpf(): string {
     return this.cpf;
@@ -33,15 +35,18 @@ class UserAccount {
   public setAge(): number {
     return this.age;
   }
-}
+  public getTransaction():Transaction[]{
+return this.transactions
+  }
+  public setTransaction():Transaction[]{
+    return this.transactions
+  }
+  
+  }
 
-const pessoa: UserAccount = new UserAccount(
-  "012.234.324-87",
-  "Maria da Silva",
-  34
-);
 
-console.log(pessoa.getName());
+
+
 
 //c)usando os métodos getters e setters.
 
@@ -58,19 +63,35 @@ class Transaction {
     this.date = date;
   }
 }
+const transactions: Transaction = new Transaction(
+  "pessoa fez transação",
+  50.0,
+  "02/03/2022"
+);
+const pessoa: UserAccount = new UserAccount(
+  "012.234.324-87",
+  "Maria da Silva",
+  34,
+  [transactions]
 
+);
 //Exercício 3
 
 class Bank {
-    private accounts:UserAccount[]
+  private accounts: UserAccount[];
 
-    constructor(accounts:UserAccount[]){
-        this.accounts = accounts;
-    }
-    public getAccount(): UserAccount[]{
-        return this.accounts
-    }
-    public setAccount():UserAccount[]{
-        return this.accounts
-    }
+  constructor(accounts: UserAccount[]) {
+    this.accounts = accounts;
+  }
+  public getAccount(): UserAccount[] {
+    return this.accounts;
+  }
+  public setAccount(): UserAccount[] {
+    return this.accounts;
+  }
 }
+console.log(pessoa.getName());
+
+const newBank: Bank = new Bank([pessoa]);
+
+console.log(newBank.getAccount())
